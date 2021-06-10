bb35f8bc53e8d3cbb6394a17f55ca436e1dd45cd
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 15:25:57 +0200

    1ere version de l'exercice

---
+++ exercice4.4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 4.4</title>
+</head>
+<body>
+    <h1>Exercice 4 Partie 4 : Les fonctions PHP</h1>
+    <?php
+    $number1 = 12;
+    $number2 = 8;
+    function result($number1,$number2) : string {
+        if ($number1 > $number2)
+        {
+            return "$number1 est plus grand que $number2";
+        }
+        if ($number1 < $number2)
+        {
+            return "$number1 est plus petit que $number2";
+        }
+        return "$number1 est égale à $number2";
+    }
+    ?>  
+</body>
+</html>
